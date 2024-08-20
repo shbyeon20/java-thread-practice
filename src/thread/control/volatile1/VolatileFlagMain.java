43578e33bf44e998e9eff3e2577f1a650a456720
byeonsanghwa <sh.byeon20@gmail.com>
2024-08-20 20:52:07 +0900

    str

--- src/thread/control/volatile1/VolatileFlagMain.java
+++ src/thread/control/volatile1/VolatileFlagMain.java
@@ -1,27 +1,37 @@
 package thread.control.volatile1;
 
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
 public class VolatileFlagMain {
     public static void main(String[] args) {
-        Mytask mytask = new Mytask();
-        Thread t1 = new Thread(mytask);
+        Mytask task = new Mytask();
+        Thread thread = new Thread(task);
+        log("runflag = "+task.runFlag);
+        thread.start();
 
-
-
+        sleep(1000);
+        task.runFlag = false;
+        log("runflag = "+task.runFlag);
+        log("main 종료");
 
 
     }
 
     private static class Mytask implements Runnable {
 
-        boolean runFlag = true;
+        volatile boolean runFlag = true;
 
 
         @Override
         public void run() {
-            log("task 시작")
+            log("task 시작");
             while (runFlag) {
-
+                //run flag가 false가 되면 탈출
+                System.out.println("hello");
+                sleep(1000);
             }
+            log("task 종료");
 
         }
     }
