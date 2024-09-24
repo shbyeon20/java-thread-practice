622eb0c73f375c9a5c392f4afd71d30c6d574723
Byeon-Sanghwa <bsh6226@gmail.com>
2024-09-24 10:08:24 +0900

    str

---
+++ src/thread/sync/test/Immutable.java
@@ -0,0 +1,12 @@
+package thread.sync.test;
+
+public class Immutable {
+    private final int value;
+    public Immutable(int value) {
+        this.value = value;
+    }
+    public int getValue() {
+        return value;
+    }
+
+}
